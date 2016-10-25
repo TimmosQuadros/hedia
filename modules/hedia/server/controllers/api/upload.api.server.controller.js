bcade78e6e5928e8c7cee72f4bf3752e46795558
alex <alex.statkevich@gmail.com>
2016-10-25 12:06:02 +0300

    web service avatar-upload. swagger documentation

--- modules/hedia/server/controllers/api/upload.api.server.controller.js
+++ modules/hedia/server/controllers/api/upload.api.server.controller.js
@@ -17,7 +17,7 @@ var _ = require('lodash'),
 
 exports.uploadImage = function(req, res) {
   var user = req.user;
-  var upload = multer(config.uploads.profileUpload).single('avatar');
+  var upload = multer(config.uploads.profileUpload).single('newProfilePicture');
   var profileUploadFileFilter = require(path.resolve('./config/lib/multer')).profileUploadFileFilter;
   var existingImageUrl;
 

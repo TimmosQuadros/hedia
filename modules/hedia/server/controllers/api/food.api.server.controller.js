55f7ef2166206539c545f2e8749d036dd31c0723
GitWaqas <waqas@hedia.dk>
2017-08-10 14:09:13 +0200

    food api

--- modules/hedia/server/controllers/api/food.api.server.controller.js
+++ modules/hedia/server/controllers/api/food.api.server.controller.js
@@ -7,7 +7,8 @@ var path = require('path'),
   mongoose = require('mongoose'),
   Food = mongoose.model('Food'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
-  _ = require('lodash');
+  _ = require('lodash'),
+  validator = require('validator');
 
 var safeFoodObject = function (food) {
   return {

58b0e552af0197e33cad4f0be963e1bf9c0c3069
Waqas Mohiuddin <waqas@Lumi.home>
2018-01-20 15:12:38 +0100

    food stuff

--- modules/hedia/server/controllers/api/upload.api.server.controller.js
+++ modules/hedia/server/controllers/api/upload.api.server.controller.js
@@ -92,6 +92,7 @@ exports.uploadImage = function(req, res, next) {
 //************startOfFood******************
 exports.uploadFoodImage = function(req, res, next) {
   var food = req.body.food;
+  console.log("Timmy er en abe fordi jeg siger det, og derfor passer det", ++food)
   var upload = multer(config.uploads.foodUpload).single('newFoodPicture');
   var profileUploadFileFilter = require(path.resolve('./config/lib/multer')).profileUploadFileFilter;
   var existingImageFoodUrl;
